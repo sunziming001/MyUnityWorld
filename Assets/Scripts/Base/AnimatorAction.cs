47d59269649a442509ed86151281abb7efd8a87e
sunziming001 <sunziming001@126.com>
2020-01-10 12:54:51 +0800

    add default modle

---
+++ Assets/Scripts/Base/AnimatorAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCtrl
+{
+
+	public class AnimatorAction : AbsAction
+	{
+		private static string KeyIsMoving = "IsMoving";
+
+		AnimatorAction()
+		{
+			SetIsBreakableByInput(true);
+		}
+
+		public static void SetIsMoving(in ActionParam param, bool v)
+		{
+			param.PutParam(AnimatorAction.KeyIsMoving, v);
+		}
+
+		public static bool GetIsMoving(ActionParam param)
+		{
+			return param.GetParam<bool>(AnimatorAction.KeyIsMoving);
+		}
+
+		protected override void OnActionExecute()
+		{
+			base.OnActionExecute();
+
+			ActionParam param = GetActionParam();
+			bool isMoving = GetIsMoving(param);
+
+			Animator anim = GetComponent<Animator>();
+			anim.SetBool("IsMoving", isMoving);
+		}
+	}
+}
---
+++ Assets/Scripts/Base/CharacterInputCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCtrl;
+
+public class CharacterInputCtrl :  AbsInputCtrl
+{
+	void Awake()
+	{
+		InitialAction2InputJudge();
+	}
+
+	void FootR()
+	{
+
+	}
+
+	void FootL()
+	{
+
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		executeActions();
+	}
+
+	void MoveInput(in ActionParam param)
+	{
+		AnimatorAction.SetActionParamValid(param, false);
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			AnimatorAction.SetActionParamValid(param, true);
+			AnimatorAction.ClearActionParam(param);
+		}
+	}
+
+	protected override void InitialAction2InputJudge()
+	{
+		AnimatorAction moveAction = GetComponent<AnimatorAction>();
+		appendAction2InputJudge(moveAction, MoveInput);
+	}
+}
