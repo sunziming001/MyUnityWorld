0541a148153b8e7a52f9f2809f469f02c66f66a4
sunziming001 <sunziming001@126.com>
2020-01-15 19:40:03 +0800

    fix warnning

--- Assets/Scripts/Test/TestLogic.cs
+++ Assets/Scripts/Test/TestLogic.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class TestLogic : MonoBehaviour
 {
 	private GameObject character;
-	private Camera mainCamera;
+	private Camera mainCamera = null;
     // Start is called before the first frame update
     void Awake()
     {
-		GameObject characterRes = Resources.Load<GameObject>("Character/RPG-Character");
-		UnityEditor.Animations.AnimatorController animatorController =  Resources.Load("Animator/CommonAnimatorController") as UnityEditor.Animations.AnimatorController;
-		mainCamera = Instantiate(gameObject.AddComponent<Camera>(), new Vector3(0, 30, 0), Quaternion.identity) as Camera;
 
-		if (characterRes 
+		initUserCtrlCharacter();
+	}
+
+	private void initUserCtrlCharacter()
+	{
+		GameObject characterRes = Resources.Load<GameObject>("Character/RPG-Character");
+		UnityEditor.Animations.AnimatorController animatorController = Resources.Load("Animator/CommonAnimatorController") as UnityEditor.Animations.AnimatorController;
+		mainCamera = gameObject.AddComponent<Camera>();
+
+		if (mainCamera != null)
+		{
+			mainCamera.transform.position = new Vector3(0, 30, 0);
+			mainCamera.transform.rotation = Quaternion.identity;
+		}
+
+		if (characterRes
 			&& animatorController
 			&& mainCamera)
 		{
@@ -22,28 +34,18 @@ public class TestLogic : MonoBehaviour
 			mainCamera.transform.localPosition = character.transform.localPosition + new Vector3(0, 6, -5);
 			mainCamera.transform.Rotate(new Vector3(30, 0, 0), Space.Self);
 
-			character.transform.localPosition = new Vector3(32, 0, 32);
+			character.transform.localPosition = new Vector3(0, 0, 0);
 			character.transform.localScale = new Vector3(5, 5, 5);
 
 			character.AddComponent<CharacterInputCtrl>();
 			Animator animator = character.GetComponent<Animator>();
 
-			if(animator)
+			if (animator)
 			{
 				animator.runtimeAnimatorController = animatorController;
 
 			}
 
-
-
-
 		}
-
 	}
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
